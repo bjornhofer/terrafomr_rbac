32c8b121275daae24c51283676759fe49d1a70be
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-06 16:18:03 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,19 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
 variable "base_name" {
   type        = string
   description = "Base Name for the groups to create (eg. project-name)"
