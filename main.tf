af88fb72dcae77744cc745f6cd02157eb79f276d
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-04-25 13:54:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "azuread_client_config" "current" {}
 // create groups
 resource "azuread_group" "main" {
   for_each         = var.user_role_map
-  display_name     = "${var.base_name}_${each.value.name}"
+  display_name     = "${var.base_name}-${each.value.name}"
   owners           = var.owner == null ? [data.azuread_client_config.current.object_id] : var.owner
   security_enabled = true
   //provider = azurerm.rbac
